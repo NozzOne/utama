3ef4f3720e57106562184635c35535dfb48c6143
NozzOne <nozztutos@gmail.com>
2022-01-09 03:36:17 -0300

    minor update

--- animefly/urls.py
+++ animefly/urls.py
@@ -17,17 +17,22 @@ from django.contrib import admin
 from django.urls import path
 from .utama import views
 
+handler500 = "animefly.utama.views.error_500"
+handler404 = "animefly.utama.views.error_404"
+handler403 = "animefly.utama.views.error_403"
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('anime/<int:id>/<str:nombre>', views.anime, name='anime'),
     path('ver/<int:id>/<str:nombre>', views.ver, name='ver'),
+    path('directorio/', views.directorio, name="directorio"),
+    path('directorio/<str:genero>/', views.directorio_genero, name="directorio_genero"),
+
     path('media/render/<str:filename>', views.render_image, name='render'),
     path('media/background/<str:filename>', views.background_image, name='background'),
     path('media/cover/<str:filename>', views.cover_image, name='cover'),
     path('media/episode/<str:filename>', views.episode_image, name='episode'),
-
-
 
 ]
--- animefly/utama/views.py
+++ animefly/utama/views.py
@@ -17,14 +17,36 @@ def anime(request, id, nombre=None):
     RenderAnime = AnimeRender.objects.get(anime_id=id).filename
     episodios = EpisodeThumbnail.objects.select_related('episode').filter(episode__anime=id).order_by('-id')[0:12]
     count_episodes = Episode.objects.filter(anime_id=id).count()
-    return render(request, 'utama/anime.html', {'anime': anime, 'capitulos': episodios, 'generos': anime_genres, 'render': RenderAnime, 'count': count_episodes})
+    # get cover with anime id
+    cover = AnimeCover.objects.get(anime_id=id).filename
+
+    return render(request, 'utama/anime.html', {'anime': anime, 'capitulos': episodios, 'generos': anime_genres, 'render': RenderAnime, 'count': count_episodes, 'cover': cover})
 
 
 def ver(request, id, nombre=None):
     episode = Episode.objects.get(id=id)
     anime_episode = AnimeEpisode.objects.filter(episode_id=id).order_by('server')
-    return render(request, 'utama/ver.html', {'episode': episode, 'servers': anime_episode})
+    # get previus episode
+    prev = Episode.objects.filter(anime_id=episode.anime_id).filter(id__lt=episode.id).order_by('-id')[0:1]
+    # get next episode
+    next = Episode.objects.filter(anime_id=episode.anime_id).filter(id__gt=episode.id).order_by('id')[0:1]
+    # obtener animes con categoria similar
+    # anime_genres = AnimeGenres.objects.select_related('genre').filter(anime_id=episode.anime_id)
+    # similar = [i.genre.id for i in anime_genres][0]
+    # # get anime similar
+    # anime_similar = AnimeGenres.objects.filter(genre=similar).exclude(anime_id=episode.anime_id).distinct()[0:5]
 
+    return render(request, 'utama/ver.html', {'episode': episode, 'servers': anime_episode, 'prev': prev, 'next': next})
+
+def directorio(request):
+    # genres order by name
+    generos = Genres.objects.order_by('name')
+    return render(request, 'utama/directorio.html', {'generos': generos})
+
+def directorio_genero(request, id, nombre=None):
+    genero = Genres.objects.get(id=id)
+    animes = AnimeGenres.objects.select_related('anime').filter(genre=genero).order_by('-id')
+    return render(request, 'utama/directorio.html', {'animes': animes, 'genero': genero})
 
 def render_image(request, filename):
     path = AnimeRender.objects.get(filename=filename).render.path
@@ -42,3 +64,14 @@ def cover_image(request, filename):
 def episode_image(request, filename):
     path = EpisodeThumbnail.objects.get(filename=filename).thumbnail.path
     return FileResponse(open(path, 'rb'), content_type='image/webp')
+
+
+def error_500(request, exception=None):
+    return render(request, 'utama/500.html', {})
+
+def error_404(request, exception):
+    return render(request, 'utama/404.html', {})
+
+def error_403(request, exception=None):
+    return render(request, 'utama/403.html', {})
+
