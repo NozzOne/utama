31d75f89172a41baefba530dac9b90b3dce5fe5f
NozzOne <nozztutos@gmail.com>
2022-01-19 05:05:19 -0300

    Update admin.py

--- animefly/utama/admin.py
+++ animefly/utama/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import Anime, Genres, AnimeGenres,  Episode, AnimeEpisode
+from import_export.admin import ImportExportModelAdmin
+
+class AnimeEpisodeResource(ImportExportModelAdmin):
+    model = AnimeEpisode
 
 # Register your models here.
 admin.site.register(Anime)
 admin.site.register(Genres)
 admin.site.register(AnimeGenres)
 admin.site.register(Episode)
-admin.site.register(AnimeEpisode)
+admin.site.register(AnimeEpisode, AnimeEpisodeResource)
 
