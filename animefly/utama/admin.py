04fff8a8352ee85c5efe967618c880f9b9430f74
NozzOne <nozztutos@gmail.com>
2022-01-20 04:53:28 -0300

    Update admin.py

--- animefly/utama/admin.py
+++ animefly/utama/admin.py
@@ -1,20 +1,23 @@
+import imp
 from django.contrib import admin
 from .models import Anime, Genres, AnimeGenres,  Episode, AnimeEpisode
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+from import_export.widgets import ForeignKeyWidget
 
 
 class AnimeEpisodeResource(resources.ModelResource):
 
-    anime_id = resources.Field(attribute='anime_id', column_name='anime_id')
-    episode_id = resources.Field(attribute='episode_id', column_name='episode_id')
+    id = resources.Field(attribute='id')
+    anime = resources.Field(attribute='anime', column_name='anime', widget=ForeignKeyWidget(Anime, 'id'))
+    episode = resources.Field(attribute='episode', column_name='episode', widget=ForeignKeyWidget(Episode, 'id'))
     server = resources.Field(attribute='server', column_name='server')
     link = resources.Field(attribute='link', column_name='link')
 
     class Meta:
         model = AnimeEpisode
-        import_id_fields = ('anime_id', 'episode_id')
-        fields = ('anime_id', 'episode_id', 'server', 'link')
+        import_id_fields = ('id','anime', 'episode')
+        fields = ('id','anime', 'episode', 'server', 'link')
 
 class AnimeEpisodeAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ("anime", "episode", "server", "link")
